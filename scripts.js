6e4c8686ba050eb14229366e71829b98ac6b6be0
JanekMoz <mozyr12@gmail.com>
2017-01-08 16:39:18 +0100

    Wprowadzono poprawki

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 // plik scripts.js
-var a='30'
-var b='100'
-var triangleArea ='a*h/2'
+var a = 30,
+	b = 100;
+var triangleArea = a*h/2
 console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
